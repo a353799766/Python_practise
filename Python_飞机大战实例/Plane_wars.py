cb803194e4380584cb69c1ad63ae3662d4bd1711
Kyo <cksir520@gmail.com>
2018-11-26 19:26:30 +0800

    修改了素材大小和加载图片的路径

--- Python_飞机大战实例/Plane_wars.py
+++ Python_飞机大战实例/Plane_wars.py
@@ -7,13 +7,17 @@ import pygame
 
 def main():
     # 1.创建一个窗口，用来显示内容
-    screen = pygame.display.set_mode((480, 852), 0, 32)
+    screen = pygame.display.set_mode((640, 1136), 0, 32)
     # 2.创建一个跟窗口大小一致的图片，用来填充当背景
-    background = pygame.image.load("./Resource/background.jpg")
-#     3.把背景图片放到窗口中显示
+    background = pygame.image.load("./spritesheets/background_2.png")
+    # 创建一个我方飞机的图片，和上面一样
+    hero = pygame.image.load("./spritesheets/hero_fly_1.png")
+    # 3.把背景图片放到窗口中显示
     while True:
-        # 设定需要显示的背景图在窗口中哪个位置显示
+        # 设定需要显示的图在窗口中哪个位置显示
         screen.blit(background, (0, 0))
+        screen.blit(hero, (252, 968))
+
         # 更新需要显示的内容
         pygame.display.update()
         # time.sleep(5)
