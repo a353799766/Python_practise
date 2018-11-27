05bbb4fb37eca143fa5726e5f08caadf805de5a1
Kyo <cksir520@gmail.com>
2018-11-27 14:00:16 +0800

    import pygame.locals 来监听键盘动作，主要设定了4个方向键，并且用面向过程的方式实现了飞机上下左右的移动

--- Python_飞机大战实例/Plane_wars.py
+++ Python_飞机大战实例/Plane_wars.py
@@ -3,6 +3,7 @@
 
 import pygame
 # import time
+from pygame.locals import *  # 用来检测事件，比如键盘按键操作
 
 
 def main():
@@ -13,11 +14,37 @@ def main():
     # 创建一个我方飞机的图片，和上面一样
     hero = pygame.image.load("./spritesheets/hero_fly_1.png")
     # 3.把背景图片放到窗口中显示
+    x = 252
+    y = 968
     while True:
         # 设定需要显示的图在窗口中哪个位置显示
         screen.blit(background, (0, 0))
-        screen.blit(hero, (252, 968))
-
+        screen.blit(hero, (x, y))
+        # 获取事件，比如按键等
+        for event in pygame.event.get():
+            # 判断是否是点击了退出按钮
+            if event.type == QUIT:
+                print("exit")
+                exit()
+            # 判断是否是按下了键
+            elif event.type == KEYDOWN:
+                # 检测按键是否是a或者left
+                if event.key == K_a or event.key == K_LEFT:
+                    print('left')
+                    x -= 5
+                # 检测按键是否是d或者方向键right
+                elif event.key == K_d or event.key == K_RIGHT:
+                    print('right')
+                    x += 5
+                elif event.key == K_w or event.key == K_UP:
+                    print('up')
+                    y -= 5
+                elif event.key == K_s or event.key == K_DOWN:
+                    print('down')
+                    y += 5
+                # 检测按键是否是空格键
+                elif event.key == K_SPACE:
+                    print('space')
         # 更新需要显示的内容
         pygame.display.update()
         # time.sleep(5)
