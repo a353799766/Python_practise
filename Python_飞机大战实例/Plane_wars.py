2d883fc38a271047c710ba07a9de906a8702cb53
Kyo <cksir520@gmail.com>
2018-11-27 15:45:26 +0800

    更改图片大小

--- Python_飞机大战实例/Plane_wars.py
+++ Python_飞机大战实例/Plane_wars.py
@@ -8,14 +8,14 @@ from pygame.locals import *  # 用来检测事件，比如键盘按键操作
 
 def main():
     # 1.创建一个窗口，用来显示内容
-    screen = pygame.display.set_mode((640, 1136), 0, 32)
+    screen = pygame.display.set_mode((320, 568), 0, 32)
     # 2.创建一个跟窗口大小一致的图片，用来填充当背景
     background = pygame.image.load("./spritesheets/background_2.png")
     # 创建一个我方飞机的图片，和上面一样
     hero = pygame.image.load("./spritesheets/hero_fly_1.png")
     # 3.把背景图片放到窗口中显示
-    x = 252
-    y = 968
+    x = 140
+    y = 488
     while True:
         # 设定需要显示的图在窗口中哪个位置显示
         screen.blit(background, (0, 0))
