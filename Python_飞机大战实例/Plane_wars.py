158e2c297e80125ae312f1e8ee5b57844f23aa5e
Kyo <cksir520@gmail.com>
2018-11-27 19:13:00 +0800

    初步定义了子弹类，但是还未实现子弹的其他功能

--- Python_飞机大战实例/Plane_wars.py
+++ Python_飞机大战实例/Plane_wars.py
@@ -2,7 +2,7 @@
 
 
 import pygame
-# import time
+import time
 from pygame.locals import *  # 用来检测事件，比如键盘按键操作
 
 
@@ -32,7 +32,18 @@ class Hero(object):
         self.y -= 5
 
 
-def key_control(hero_temp):
+class Bullets(object):
+    def __init__(self):
+        self.x = 140
+        self.y = 488
+        self.image = pygame.image.load(
+            "./spritesheets/bullet1.png")  # 创建一个子弹图片，和上面一样
+
+    def display(self, screen_temp):
+        screen_temp.blit(self.image, (self.x, self.y))
+
+
+def key_control(hero_temp,):
     """控制键盘的函数"""
     for event in pygame.event.get():
         # 判断是否是点击了退出按钮
@@ -58,6 +69,7 @@ def key_control(hero_temp):
             # 检测按键是否是空格键
             elif event.key == K_SPACE:
                 print('space')
+                pass  # 这里应该是飞机.显示子弹方法（）
 
 
 def main():
@@ -66,6 +78,7 @@ def main():
     # 2.创建一个跟窗口大小一致的图片，用来填充当背景
     background = pygame.image.load("./spritesheets/background_2.png")
     hero = Hero()  # 创建我方飞机英雄对象
+    bullets = Bullets()
     while True:
         # 设定需要显示的图在窗口中哪个位置显示
         screen.blit(background, (0, 0))
@@ -75,7 +88,7 @@ def main():
         key_control(hero)
         # 更新需要显示的内容
         pygame.display.update()
-        # time.sleep(5)
+        time.sleep(0.01)
 
 
 if __name__ == '__main__':
